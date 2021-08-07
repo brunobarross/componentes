a04263629ab418791ca734699b0d19a77e5460f8
Altamiro Bruno <62890135+brunobarross@users.noreply.github.com>
2021-08-07 12:03:18 -0300

    ajustes finais

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,21 +1,55 @@
-function Dom(elemento, classe){
+function Dom(elemento){
     const elementoSelecionado = document.querySelector(elemento)
     this.element = elementoSelecionado;
 
-    this.addClasse = function (){
-        this.element.classList.add(classe)
+    this.addClasse = function (classe){
+        elementoSelecionado.classList.toggle(classe);
+    }
+
+    this.removeClasse = function (classe){
+        elementoSelecionado.classList.remove(classe);
     }
 }
 
-const adicionarClasse = new Dom();
 
-function barraBusca(elemento){
-    const lupa = document.querySelector(elemento);
-    lupa.addEventListener('click', ativarBusca);
-    function ativarBarra(){
+
+function barraBusca(){
+    const adicionarClasse = new Dom('.menu');
+
+const lupa = document.querySelector('.lupa');
+
+lupa.addEventListener("click", ()=>{
+    adicionarClasse.addClasse('ativar');
+});
+}
+
+barraBusca();
+
+/******ACCORDION ******/
+
+function accordion(){
+    const adicionarClasse = new Dom('.faq');
+    const perguntas = document.querySelectorAll('.faq__wrapper-item h3');
+
+    perguntas.forEach((pergunta) =>{
+        pergunta.addEventListener('click', mostraResposta)
+    });
+
+
+    function mostraResposta(evento){
+  
 
     }
+
 }
+
+accordion(console.log('iniciei'));
+
+
+
+
+
+
 
 
 
